637898bc089a2ab769c75e5fab34e56d48a7e138
PROTOTYPE9111 <prototype9111@list.ru>
2025-05-01 20:08:05 +0300

    добавил файлы

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,35 @@
 #include <SFML/Graphics.hpp>
+#include <optional>
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
+    sf::RenderWindow window(sf::VideoMode({800u, 600u}), "SFML 3.x Example");
+
+    sf::CircleShape circle(100.f);
+    circle.setFillColor(sf::Color::Red);
+    circle.setPosition({350.f, 250.f});
 
     while (window.isOpen())
     {
+        // Используем std::optional для события
+        std::optional<sf::Event> event;
         while (const std::optional event = window.pollEvent())
         {
             if (event->is<sf::Event::Closed>())
             {
                 window.close();
             }
+            else if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>())
+            {
+                if (keyPressed->scancode == sf::Keyboard::Scancode::Escape)
+                    window.close();
+            }
         }
-
-        window.clear();
+        
+        window.clear(sf::Color::Black);
+        window.draw(circle);
         window.display();
     }
+
+    return 0;
 }
